0e2e6dd1f25dd50f5e5aaa93e7329b3044a0a21d
etctera <yilonglei790@gmail.com>
2024-01-15 15:03:37 +0800

    feat: code formatter

--- models/Airdrop.ts
+++ models/Airdrop.ts
@@ -1,29 +1,26 @@
-import * as borsh from "@project-serum/borsh"
+import * as borsh from '@project-serum/borsh'
 
 export class AirdropSchema {
-    amount: number
+  amount: number
 
-    constructor(amount: number) {
-        this.amount = amount
+  constructor(amount: number) {
+    this.amount = amount
+  }
+
+  AIRDROP_IX_DATA_LAYOUT = borsh.struct([borsh.u8('variant'), borsh.u32('amount')])
+
+  serialize(): Buffer {
+    // const buffer = Buffer.alloc(1000)
+    // this.AIRDROP_IX_DATA_LAYOUT.encode({ ...this, variant: 0 }, buffer)
+    // return buffer.slice(0, this.AIRDROP_IX_DATA_LAYOUT.getSpan(buffer))
+    const payload = {
+      variant: 0,
+      amount: this.amount,
     }
 
-    AIRDROP_IX_DATA_LAYOUT = borsh.struct([
-        borsh.u8("variant"),
-        borsh.u32("amount"),
-    ])
+    const ixBuffer = Buffer.alloc(9)
+    this.AIRDROP_IX_DATA_LAYOUT.encode(payload, ixBuffer)
 
-    serialize(): Buffer {
-        // const buffer = Buffer.alloc(1000)
-        // this.AIRDROP_IX_DATA_LAYOUT.encode({ ...this, variant: 0 }, buffer)
-        // return buffer.slice(0, this.AIRDROP_IX_DATA_LAYOUT.getSpan(buffer))
-        const payload = {
-            variant: 0,
-            amount: this.amount,
-        }
-
-        const ixBuffer = Buffer.alloc(9)
-        this.AIRDROP_IX_DATA_LAYOUT.encode(payload, ixBuffer)
-
-        return ixBuffer
-    }
+    return ixBuffer
+  }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
-import "../styles/globals.css"
-import type { AppProps } from "next/app"
-import { ChakraProvider } from "@chakra-ui/react"
-import WalletContextProvider from "../components/WalletContextProvider"
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+import WalletContextProvider from '../components/WalletContextProvider'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <ChakraProvider>
-            <WalletContextProvider>
-                <Component {...pageProps} />
-            </WalletContextProvider>
-        </ChakraProvider>
-    )
+  return (
+    <ChakraProvider>
+      <WalletContextProvider>
+        <Component {...pageProps} />
+      </WalletContextProvider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
